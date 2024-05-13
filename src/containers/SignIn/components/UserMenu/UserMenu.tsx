dc6e5ecbcff9ff617571095710dc450abc8fdf39
Alex Nepsha <nepsha1986@gmail.com>
2024-05-13 15:26:40 +0300

    feature. improved UserMenu

--- src/containers/SignIn/components/UserMenu/UserMenu.tsx
+++ src/containers/SignIn/components/UserMenu/UserMenu.tsx
@@ -1,30 +1,38 @@
 "use client";
-
+import Link from "next/link";
 import { UserOutlined } from "@ant-design/icons";
-import { Dropdown, MenuProps, Space } from "antd";
+import { Dropdown, MenuProps } from "antd";
+import { usePathname } from "next/navigation";
 
-const items: MenuProps["items"] = [
-  {
-    key: "1",
-    label: <a href="/api/auth/logout">Logout</a>,
-  },
-];
+const getMenuItems = (curPath: string): MenuProps["items"] => {
+  return [
+    {
+      key: "1",
+      label: !curPath.includes("/dashboard") ? (
+        <Link href="/dashboard">Dashboard</Link>
+      ) : (
+        <Link href="/">Homepage</Link>
+      ),
+    },
+    {
+      key: "2",
+      label: <a href="/api/auth/logout">Logout</a>,
+    },
+  ];
+};
 
 interface Props {
   userName: string;
 }
 
 const UserMenu: React.FC<Props> = ({ userName }) => {
+  const currentPath = usePathname();
+  const items = getMenuItems(currentPath);
+
   return (
-    <Dropdown
-      menu={{ items }}
-      placement="bottom"
-      trigger={["click"]}
-    >
+    <Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
       <a onClick={(e) => e.preventDefault()}>
-        <Space>
-          <UserOutlined /> {userName}
-        </Space>
+        <UserOutlined /> {userName}
       </a>
     </Dropdown>
   );
