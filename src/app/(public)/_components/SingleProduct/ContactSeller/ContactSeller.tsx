d6efb4edfeda7182612964835d49eb2b2cc2f937
Alex Nepsha <nepsha1986@gmail.com>
2024-06-05 14:16:58 +0300

    feature. get vendor contact data. draft

---
+++ src/app/(public)/_components/SingleProduct/ContactSeller/ContactSeller.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { vendorService } from "@/app/(public)/_services/vendor.service";
+
+interface Props {
+  id: string;
+}
+
+const ContactSeller = ({ id }: Props) => {
+  const { data: vendor, refetch } = useQuery({
+    queryKey: ["getSellerInfo", id],
+    queryFn: () => {
+      return vendorService.get(id);
+    },
+    enabled: false,
+  });
+
+  const handleClick = () => {
+    refetch();
+  };
+
+  return (
+    <>
+      {!!vendor ? (
+        <div> Email: {vendor.email}</div>
+      ) : (
+        <Button
+          type="primary"
+          size="large"
+          style={{ marginTop: "2rem" }}
+          onClick={handleClick}
+        >
+          Contact the Seller
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default ContactSeller;
---
+++ src/app/api/v1/(public)/vendors/[id]/route.ts
@@ -0,0 +1,20 @@
+import db from "@/backend/db";
+import { NextRequest } from "next/server";
+import { ObjectId } from "mongodb";
+
+export async function GET(_: NextRequest, context: { params: { id: string } }) {
+  try {
+    const _id = context.params.id;
+    const query = { _id };
+
+    // TODO: handle 404
+    const client = await db;
+    const collection = client.db("flowers_app").collection("profiles");
+    // @ts-ignore
+    const result = await collection.findOne(query);
+
+    return new Response(JSON.stringify(result));
+  } catch (e) {
+    return new Response("Error! Please try again later", { status: 500 });
+  }
+}
